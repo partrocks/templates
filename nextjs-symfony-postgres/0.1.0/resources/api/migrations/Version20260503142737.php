2c4839353b20c1a88edb605b30335a3aa8b5b739
Paul Rooney <paulroon@gmail.com>
2026-05-03 16:24:11 +0100

    update

--- nextjs-symfony-postgres/0.1.0/resources/api/migrations/Version20260503142737.php
+++ nextjs-symfony-postgres/0.1.0/resources/api/migrations/Version20260503142737.php
@@ -19,14 +19,11 @@ final class Version20260503142737 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE quotes (id SERIAL NOT NULL, content TEXT NOT NULL, author VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE quotes');
     }
 }
